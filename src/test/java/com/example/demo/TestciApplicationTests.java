329219bbc955df6d48ddddd65dfcaef455dd82bb
chenwt <feiniaoying@163.com>
2017-09-22 15:23:49 +0800

    //测试案例

--- src/test/java/com/example/demo/TestciApplicationTests.java
+++ src/test/java/com/example/demo/TestciApplicationTests.java
@@ -1,16 +1,14 @@
 package com.example.demo;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestciApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		System.out.println("test通过！！！");
 	}
 
 }
