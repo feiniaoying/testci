eeeb9cd8c2b1f339c5fa3854f101643dca530aec
chenwt <feiniaoying@163.com>
2017-09-22 11:54:24 +0800

    //测试1

--- src/main/java/com/example/demo/TestciApplication.java
+++ src/main/java/com/example/demo/TestciApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestciApplication {
 
 	public static void main(String[] args) {
+		System.out.println("测试！");
 		SpringApplication.run(TestciApplication.class, args);
 	}
 }
